5057799f552f6459eb8fbca70dd94ccecc493150
N S <kwb020312@naver.com>
2023-05-21 23:50:41 +0900

    Feat:Add Tile

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ canvas.height = 576;
 
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image();
+image.src = "./img/Pellet Town.png";
+
+console.log(image);
+
+image.onload = () => {
+  c.drawImage(image, 0, 0);
+};
