4d8789ab372ac256e961b9e03d5dd3a5e0a41864
N S <kwb020312@naver.com>
2023-05-23 21:22:18 +0900

    Fix: 불필요 코드 제거

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ const c = canvas.getContext("2d");
 canvas.width = 1024;
 canvas.height = 576;
 
-c.fillStyle = "white";
-c.fillRect(0, 0, canvas.width, canvas.height);
-
 const image = new Image();
 image.src = "./img/Pellet Town.png";
 
