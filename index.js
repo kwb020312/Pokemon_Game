6fc858f6d8c2ba3a005b04ade416ac3c0d73bf26
N S <kwb020312@naver.com>
2023-05-24 23:08:03 +0900

    Feat: 전투 감지 리스너 조건 추가

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ canvas.width = 1024;
 canvas.height = 576;
 
 const collisionsMap = [];
-
 for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, 70 + i));
+}
+
+const battleZonesMap = [];
+for (let i = 0; i < battleZonesData.length; i += 70) {
+  battleZonesMap.push(battleZonesData.slice(i, 70 + i));
 }
 
 const boundaries = [];
@@ -20,6 +24,22 @@ collisionsMap.forEach((row, i) => {
   row.forEach((symbol, j) => {
     if (symbol === 1025)
       boundaries.push(
+        new Boundary({
+          position: {
+            x: j * Boundary.width + offset.x,
+            y: i * Boundary.height + offset.y,
+          },
+        })
+      );
+  });
+});
+
+const battleZones = [];
+
+battleZonesMap.forEach((row, i) => {
+  row.forEach((symbol, j) => {
+    if (symbol === 1025)
+      battleZones.push(
         new Boundary({
           position: {
             x: j * Boundary.width + offset.x,
@@ -65,8 +85,6 @@ const player = new Sprite({
   },
 });
 
-console.log(player);
-
 const background = new Sprite({
   position: {
     x: offset.x,
@@ -98,7 +116,7 @@ const keys = {
   },
 };
 
-const movables = [background, ...boundaries, foreground];
+const movables = [background, ...boundaries, foreground, ...battleZones];
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
@@ -114,8 +132,39 @@ function animate() {
   boundaries.forEach((boundary) => {
     boundary.draw();
   });
+  battleZones.forEach((battleZone) => {
+    battleZone.draw();
+  });
   player.draw();
   foreground.draw();
+
+  if (keys.w.pressed || keys.a.pressed || keys.s.pressed || keys.d.pressed) {
+    for (let i = 0; i < battleZones.length; i++) {
+      const battleZone = battleZones[i];
+      const overlappingArea =
+        (Math.min(
+          player.position.x + player.width,
+          battleZone.position.x + battleZone.width
+        ) -
+          Math.max(player.position.x, battleZone.position.x)) *
+        (Math.min(
+          player.position.y + player.height,
+          battleZone.position.y + battleZone.height
+        ) -
+          Math.max(player.position.y, battleZone.position.y));
+      if (
+        rectangularCollision({
+          rectangle1: player,
+          rectangle2: battleZone,
+        }) &&
+        overlappingArea > (player.width * player.height) / 2 &&
+        Math.random() < 0.01
+      ) {
+        console.log("배틀존 입장!");
+        break;
+      }
+    }
+  }
 
   let moving = true;
   player.moving = false;
@@ -143,6 +192,7 @@ function animate() {
         break;
       }
     }
+
     if (moving)
       movables.forEach((movable) => {
         movable.position.y += 3;
@@ -170,6 +220,7 @@ function animate() {
         break;
       }
     }
+
     if (moving)
       movables.forEach((movable) => {
         movable.position.x += 3;
@@ -197,6 +248,7 @@ function animate() {
         break;
       }
     }
+
     if (moving)
       movables.forEach((movable) => {
         movable.position.y -= 3;
@@ -224,6 +276,7 @@ function animate() {
         break;
       }
     }
+
     if (moving)
       movables.forEach((movable) => {
         movable.position.x -= 3;
