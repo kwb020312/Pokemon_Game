512d4fe40bc8e211760dac9c9bf86dbfa9fac205
N S <kwb020312@naver.com>
2023-05-24 21:46:41 +0900

    Feat: 장애물에 가려지도록 수정

--- index.js
+++ index.js
@@ -10,21 +10,6 @@ for (let i = 0; i < collisions.length; i += 70) {
   collisionsMap.push(collisions.slice(i, 70 + i));
 }
 
-class Boundary {
-  static width = 48;
-  static height = 48;
-  constructor({ position }) {
-    this.position = position;
-    this.width = 48;
-    this.height = 48;
-  }
-
-  draw() {
-    c.fillStyle = "rgba(255, 0, 0, 0.1)";
-    c.fillRect(this.position.x, this.position.y, this.width, this.height);
-  }
-}
-
 const boundaries = [];
 const offset = {
   x: -745,
@@ -48,35 +33,11 @@ collisionsMap.forEach((row, i) => {
 const image = new Image();
 image.src = "./img/Pellet Town.png";
 
+const foregroundImage = new Image();
+foregroundImage.src = "./img/foregroundObjects.png";
+
 const playerImage = new Image();
 playerImage.src = "./img/playerDown.png";
-
-class Sprite {
-  constructor({ position, velocity, image, frames = { max: 1 } }) {
-    this.position = position;
-    this.image = image;
-    this.frames = frames;
-
-    this.image.onload = () => {
-      this.width = this.image.width / this.frames.max;
-      this.height = this.image.height;
-    };
-  }
-
-  draw() {
-    c.drawImage(
-      this.image,
-      0,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
-    );
-  }
-}
 
 const player = new Sprite({
   position: {
@@ -97,6 +58,14 @@ const background = new Sprite({
   image: image,
 });
 
+const foreground = new Sprite({
+  position: {
+    x: offset.x,
+    y: offset.y,
+  },
+  image: foregroundImage,
+});
+
 const keys = {
   w: {
     pressed: false,
@@ -112,7 +81,7 @@ const keys = {
   },
 };
 
-const movables = [background, ...boundaries];
+const movables = [background, ...boundaries, foreground];
 
 function rectangularCollision({ rectangle1, rectangle2 }) {
   return (
@@ -129,6 +98,7 @@ function animate() {
     boundary.draw();
   });
   player.draw();
+  foreground.draw();
 
   let moving = true;
   // 플레이어 이동 감지
