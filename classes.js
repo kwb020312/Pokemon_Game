85b392edb4bf9227488f52eee72775644c02cd7b
N S <kwb020312@naver.com>
2023-05-26 22:55:08 +0900

    Feat: 공격 모션 및 타격 모션 추가

--- classes.js
+++ classes.js
@@ -17,9 +17,12 @@ class Sprite {
     };
     this.animate = animate;
     this.sprites = sprites;
+    this.opacity = 1;
   }
 
   draw() {
+    c.save();
+    c.globalAlpha = this.opacity;
     c.drawImage(
       this.image,
       this.frames.val * this.width,
@@ -32,6 +35,8 @@ class Sprite {
       this.image.height
     );
 
+    c.restore();
+
     if (!this.animate) return;
 
     if (this.frames.max > 1) {
@@ -42,6 +47,35 @@ class Sprite {
       if (this.frames.val < this.frames.max - 1) this.frames.val++;
       else this.frames.val = 0;
     }
+  }
+
+  attack({ attack, recipient }) {
+    const tl = gsap.timeline();
+    tl.to(this.position, {
+      x: this.position.x - 20,
+    })
+      .to(this.position, {
+        x: this.position.x + 40,
+        duration: 0.1,
+        onComplete() {
+          gsap.to(recipient.position, {
+            x: recipient.position.x + 10,
+            yoyo: true,
+            repeat: 5,
+            duration: 0.08,
+          });
+
+          gsap.to(recipient, {
+            opacity: 0,
+            repeat: 5,
+            yoyo: true,
+            duration: 0.08,
+          });
+        },
+      })
+      .to(this.position, {
+        x: this.position.x,
+      });
   }
 }
 
--- index.js
+++ index.js
@@ -369,6 +369,19 @@ function animateBattle() {
 // animate();
 animateBattle();
 
+document.querySelectorAll("button").forEach((button) => {
+  button.addEventListener("click", () => {
+    emby.attack({
+      attack: {
+        name: "Tackle",
+        damage: 10,
+        type: "Normal",
+      },
+      recipient: draggle,
+    });
+  });
+});
+
 let lastKey = "";
 window.addEventListener("keydown", (e) => {
   switch (e.key) {
