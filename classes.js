aa40ff1984cc77db63dec507379d7dada95f35c9
N S <kwb020312@naver.com>
2023-05-27 13:12:52 +0900

    Feat: 상대 공격 구현

--- classes.js
+++ classes.js
@@ -6,6 +6,7 @@ class Sprite {
     frames = { max: 1, hold: 10 },
     sprites,
     animate = false,
+    isEnemy = false,
   }) {
     this.position = position;
     this.image = image;
@@ -19,6 +20,7 @@ class Sprite {
     this.sprites = sprites;
     this.opacity = 1;
     this.health = 100;
+    this.isEnemy = isEnemy;
   }
 
   draw() {
@@ -52,16 +54,26 @@ class Sprite {
 
   attack({ attack, recipient }) {
     const tl = gsap.timeline();
+
+    this.health -= attack.damage;
+
+    let movementDistance = 20;
+    if (this.isEnemy) movementDistance = -20;
+
+    let healthBar = "#enemyHealthBar";
+
+    if (this.isEnemy) healthBar = "#playerHealthBar";
+
     tl.to(this.position, {
-      x: this.position.x - 20,
+      x: this.position.x - movementDistance,
     })
       .to(this.position, {
-        x: this.position.x + 40,
+        x: this.position.x + movementDistance * 2,
         duration: 0.1,
         onComplete: () => {
           // 적 HP
-          gsap.to("#enemyHealthBar", {
-            width: this.health - attack.damage + "%",
+          gsap.to(healthBar, {
+            width: this.health + "%",
           });
           gsap.to(recipient.position, {
             x: recipient.position.x + 10,
--- index.js
+++ index.js
@@ -340,6 +340,7 @@ const draggle = new Sprite({
     hold: 30,
   },
   animate: true,
+  isEnemy: true,
 });
 
 const embyImage = new Image();
