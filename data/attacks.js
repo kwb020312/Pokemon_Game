88a145f487bf648191a82805b86d577fd447ccc7
N S <kwb020312@naver.com>
2023-05-27 17:52:11 +0900

    Feat: 공격 속성에 따른 색상 분류

--- data/attacks.js
+++ data/attacks.js
@@ -3,10 +3,12 @@ const attacks = {
     name: "몸통 박치기",
     damage: 10,
     type: "Normal",
+    color: "black",
   },
   "파이어 볼": {
     name: "파이어 볼",
     damage: 24,
     type: "Fire",
+    color: "red",
   },
 };
