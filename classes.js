a8c0865fffed0ce8f98c95740b829ffe0d50b6c2
N S <kwb020312@naver.com>
2023-05-29 17:39:05 +0900

    Fix: 생성자 분리

--- classes.js
+++ classes.js
@@ -7,43 +7,66 @@ class Sprite {
     sprites,
     animate = false,
     rotation = 0,
+    scale = 1,
   }) {
     this.position = position;
-    this.image = image;
+    this.image = new Image();
     this.frames = { ...frames, val: 0, elapsed: 0 };
-
     this.image.onload = () => {
-      this.width = this.image.width / this.frames.max;
-      this.height = this.image.height;
+      this.width = (this.image.width / this.frames.max) * scale;
+      this.height = this.image.height * scale;
     };
+    this.image.src = image.src;
+
     this.animate = animate;
     this.sprites = sprites;
     this.opacity = 1;
+
     this.rotation = rotation;
+    this.scale = scale;
   }
 
   draw() {
     c.save();
     c.translate(
       this.position.x + this.width / 2,
-      this.position.y + this.health / 2
+      this.position.y + this.height / 2
     );
     c.rotate(this.rotation);
     c.translate(
       -this.position.x - this.width / 2,
-      -this.position.y - this.health / 2
+      -this.position.y - this.height / 2
     );
     c.globalAlpha = this.opacity;
+
+    const crop = {
+      position: {
+        x: this.frames.val * (this.width / this.scale),
+        y: 0,
+      },
+      width: this.image.width / this.frames.max,
+      height: this.image.height,
+    };
+
+    const image = {
+      position: {
+        x: this.position.x,
+        y: this.position.y,
+      },
+      width: this.image.width / this.frames.max,
+      height: this.image.height,
+    };
+
     c.drawImage(
       this.image,
-      this.frames.val * this.width,
-      0,
-      this.image.width / this.frames.max,
-      this.image.height,
-      this.position.x,
-      this.position.y,
-      this.image.width / this.frames.max,
-      this.image.height
+      crop.position.x,
+      crop.position.y,
+      crop.width,
+      crop.height,
+      image.position.x,
+      image.position.y,
+      image.width * this.scale,
+      image.height * this.scale
     );
 
     c.restore();
@@ -83,45 +106,42 @@ class Monster extends Sprite {
       animate,
       rotation,
     });
-    this.name = name;
     this.health = 100;
     this.isEnemy = isEnemy;
+    this.name = name;
     this.attacks = attacks;
   }
 
   faint() {
-    document.querySelector(
-      "#dialogueBox"
-    ).innerHTML = `${this.name}가 쓰러졌습니다!`;
+    document.querySelector("#dialogueBox").innerHTML = this.name + " fainted!";
     gsap.to(this.position, {
       y: this.position.y + 20,
     });
     gsap.to(this, {
       opacity: 0,
     });
+    audio.battle.stop();
+    audio.victory.play();
   }
 
   attack({ attack, recipient, renderedSprites }) {
     document.querySelector("#dialogueBox").style.display = "block";
-    document.querySelector(
-      "#dialogueBox"
-    ).innerHTML = `${this.name}가 ${attack.name}을(를) 사용합니다!`;
+    document.querySelector("#dialogueBox").innerHTML =
+      this.name + " used " + attack.name;
 
     let healthBar = "#enemyHealthBar";
+    if (this.isEnemy) healthBar = "#playerHealthBar";
+
     let rotation = 1;
+    if (this.isEnemy) rotation = -2.2;
 
-    if (this.isEnemy) {
-      healthBar = "#playerHealthBar";
-      rotation = -2.2;
-    }
-
-    this.health -= attack.damage;
+    recipient.health -= attack.damage;
 
     switch (attack.name) {
-      case "파이어 볼":
+      case "Fireball":
+        audio.initFireball.play();
         const fireballImage = new Image();
         fireballImage.src = "./img/fireball.png";
-        console.log(fireballImage);
         const fireball = new Sprite({
           position: {
             x: this.position.x,
@@ -135,17 +155,18 @@ class Monster extends Sprite {
           animate: true,
           rotation,
         });
-
         renderedSprites.splice(1, 0, fireball);
 
         gsap.to(fireball.position, {
           x: recipient.position.x,
           y: recipient.position.y,
           onComplete: () => {
-            // 적 타격 성공시 효과
+            // Enemy actually gets hit
+            audio.fireballHit.play();
             gsap.to(healthBar, {
               width: recipient.health + "%",
             });
+
             gsap.to(recipient.position, {
               x: recipient.position.x + 10,
               yoyo: true,
@@ -164,7 +185,7 @@ class Monster extends Sprite {
         });
 
         break;
-      case "몸통 박치기":
+      case "Tackle":
         const tl = gsap.timeline();
 
         let movementDistance = 20;
@@ -177,10 +198,12 @@ class Monster extends Sprite {
             x: this.position.x + movementDistance * 2,
             duration: 0.1,
             onComplete: () => {
-              // 적 타격 성공시 효과
+              // Enemy actually gets hit
+              audio.tackleHit.play();
               gsap.to(healthBar, {
                 width: recipient.health + "%",
               });
+
               gsap.to(recipient.position, {
                 x: recipient.position.x + 10,
                 yoyo: true,
@@ -214,7 +237,35 @@ class Boundary {
   }
 
   draw() {
-    c.fillStyle = "rgba(255, 0, 0, 0.5)";
+    c.fillStyle = "rgba(255, 0, 0, 0)";
     c.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
+
+class Character extends Sprite {
+  constructor({
+    position,
+    velocity,
+    image,
+    frames = { max: 1, hold: 10 },
+    sprites,
+    animate = false,
+    rotation = 0,
+    scale = 1,
+    dialogue = [""],
+  }) {
+    super({
+      position,
+      velocity,
+      image,
+      frames,
+      sprites,
+      animate,
+      rotation,
+      scale,
+    });
+
+    this.dialogue = dialogue;
+    this.dialogueIndex = 0;
   }
 }
