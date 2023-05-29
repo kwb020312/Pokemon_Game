3576affefc5c1e688c4f3e6f9b5e9dd50bce7c98
N S <kwb020312@naver.com>
2023-05-29 17:34:00 +0900

    Fix: 몬스터 공격 방식

--- data/attacks.js
+++ data/attacks.js
@@ -1,13 +1,13 @@
 const attacks = {
-  "몸통 박치기": {
-    name: "몸통 박치기",
+  Tackle: {
+    name: "Tackle",
     damage: 10,
     type: "Normal",
     color: "black",
   },
-  "파이어 볼": {
-    name: "파이어 볼",
-    damage: 24,
+  Fireball: {
+    name: "Fireball",
+    damage: 25,
     type: "Fire",
     color: "red",
   },
