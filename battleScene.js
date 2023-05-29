9b82ac76e44e60e22b06605578c9415a6fe4b070
N S <kwb020312@naver.com>
2023-05-29 17:40:03 +0900

    Fix: 전투씬 파이어볼 오류 수정

--- battleScene.js
+++ battleScene.js
@@ -8,89 +8,124 @@ const battleBackground = new Sprite({
   image: battleBackgroundImage,
 });
 
-const draggle = new Monster(monsters.Draggle);
-
-const emby = new Monster(monsters.Emby);
-
-const renderedSprites = [draggle, emby];
-
-emby.attacks.forEach((attack) => {
-  const button = document.createElement("button");
-  button.innerHTML = attack.name;
-  document.querySelector("#attacksBox").append(button);
-});
-
+let draggle;
+let emby;
+let renderedSprites;
 let battleAnimationId;
+let queue;
 
-function animateBattle() {
-  battleAnimationId = window.requestAnimationFrame(animateBattle);
-  console.log("animate Battle");
-  battleBackground.draw();
+function initBattle() {
+  document.querySelector("#userInterface").style.display = "block";
+  document.querySelector("#dialogueBox").style.display = "none";
+  document.querySelector("#enemyHealthBar").style.width = "100%";
+  document.querySelector("#playerHealthBar").style.width = "100%";
+  document.querySelector("#attacksBox").replaceChildren();
 
-  renderedSprites.forEach((sprite) => {
-    sprite.draw();
+  draggle = new Monster(monsters.Draggle);
+  emby = new Monster(monsters.Emby);
+  renderedSprites = [draggle, emby];
+  queue = [];
+
+  emby.attacks.forEach((attack) => {
+    const button = document.createElement("button");
+    button.innerHTML = attack.name;
+    document.querySelector("#attacksBox").append(button);
   });
-}
 
-// animate();
-animateBattle();
-
-const queue = [];
-
-// '공격' 이벤트
-document.querySelectorAll("button").forEach((button) => {
-  button.addEventListener("click", (e) => {
-    const selectedAttack = attacks[e.currentTarget.innerHTML];
-    emby.attack({
-      attack: selectedAttack,
-      recipient: draggle,
-      renderedSprites,
-    });
-
-    if (draggle.health <= 0) {
-      queue.push(() => {
-        draggle.faint();
-      });
-
-      return;
-    }
-    const randomAttack =
-      draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)];
-
-    queue.push(() => {
-      draggle.attack({
-        attack: randomAttack,
-        recipient: emby,
+  // our event listeners for our buttons (attack)
+  document.querySelectorAll("button").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      const selectedAttack = attacks[e.currentTarget.innerHTML];
+      emby.attack({
+        attack: selectedAttack,
+        recipient: draggle,
         renderedSprites,
       });
 
-      if (emby.health <= 0) {
+      if (draggle.health <= 0) {
         queue.push(() => {
           draggle.faint();
         });
         queue.push(() => {
+          // fade back to black
           gsap.to("#overlappingDiv", {
             opacity: 1,
             onComplete: () => {
               cancelAnimationFrame(battleAnimationId);
               animate();
               document.querySelector("#userInterface").style.display = "none";
+
               gsap.to("#overlappingDiv", {
                 opacity: 0,
               });
+
+              battle.initiated = false;
+              audio.Map.play();
             },
           });
         });
       }
+
+      // draggle or enemy attacks right here
+      const randomAttack =
+        draggle.attacks[Math.floor(Math.random() * draggle.attacks.length)];
+
+      queue.push(() => {
+        draggle.attack({
+          attack: randomAttack,
+          recipient: emby,
+          renderedSprites,
+        });
+
+        if (emby.health <= 0) {
+          queue.push(() => {
+            emby.faint();
+          });
+
+          queue.push(() => {
+            // fade back to black
+            gsap.to("#overlappingDiv", {
+              opacity: 1,
+              onComplete: () => {
+                cancelAnimationFrame(battleAnimationId);
+                animate();
+                document.querySelector("#userInterface").style.display = "none";
+
+                gsap.to("#overlappingDiv", {
+                  opacity: 0,
+                });
+
+                battle.initiated = false;
+                audio.Map.play();
+              },
+            });
+          });
+        }
+      });
+    });
+
+    button.addEventListener("mouseenter", (e) => {
+      const selectedAttack = attacks[e.currentTarget.innerHTML];
+      document.querySelector("#attackType").innerHTML = selectedAttack.type;
+      document.querySelector("#attackType").style.color = selectedAttack.color;
     });
   });
+}
 
-  button.addEventListener("mouseenter", (e) => {
-    const selectedAttack = attacks[e.currentTarget.innerHTML];
-    document.querySelector("#attackType").innerHTML = selectedAttack.type;
-    document.querySelector("#attackType").style.color = selectedAttack.color;
+function animateBattle() {
+  battleAnimationId = window.requestAnimationFrame(animateBattle);
+  battleBackground.draw();
+
+  console.log(battleAnimationId);
+
+  renderedSprites.forEach((sprite) => {
+    sprite.draw();
   });
-});
+}
+
+animate();
+// initBattle()
+// animateBattle()
 
 document.querySelector("#dialogueBox").addEventListener("click", (e) => {
   if (queue.length > 0) {
