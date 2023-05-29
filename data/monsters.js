28879c79324f646b677e18e680c6fa9ce9f5256b
N S <kwb020312@naver.com>
2023-05-29 17:37:02 +0900

    Fix: 몬스터 배열 수정

--- data/monsters.js
+++ data/monsters.js
@@ -1,37 +1,35 @@
-const embyImage = new Image();
-embyImage.src = "./img/embySprite.png";
-
-const draggleImage = new Image();
-draggleImage.src = "./img/draggleSprite.png";
-
 const monsters = {
   Emby: {
     position: {
       x: 280,
       y: 325,
     },
-    image: embyImage,
+    image: {
+      src: "./img/embySprite.png",
+    },
     frames: {
       max: 4,
       hold: 30,
     },
     animate: true,
-    name: "불꽃이",
-    attacks: [attacks["몸통 박치기"], attacks["파이어 볼"]],
+    name: "Emby",
+    attacks: [attacks.Tackle, attacks.Fireball],
   },
   Draggle: {
     position: {
       x: 800,
       y: 100,
     },
-    image: draggleImage,
+    image: {
+      src: "./img/draggleSprite.png",
+    },
     frames: {
       max: 4,
       hold: 30,
     },
     animate: true,
     isEnemy: true,
-    name: "꼬물이",
-    attacks: [attacks["몸통 박치기"], attacks["파이어 볼"]],
+    name: "Draggle",
+    attacks: [attacks.Tackle, attacks.Fireball],
   },
 };
